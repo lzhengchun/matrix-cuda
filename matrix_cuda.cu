922a40fbd1af15d1ffd9a6e945d8b67c2f3b341a
Zhengchun Liu <liuzhengchun@gmail.com>
2015-09-20 18:37:33 +0200

    comments

--- matrix_cuda.cu
+++ matrix_cuda.cu
@@ -39,8 +39,10 @@ __global__ void gpu_matrix_mult(int *a,int *b, int *c, int m, int n, int k)
     int row = blockIdx.y * blockDim.y + threadIdx.y; 
     int col = blockIdx.x * blockDim.x + threadIdx.x;
     int sum = 0;
-    if( col < k && row < m) {
-        for(int i = 0; i < n; i++) {
+    if( col < k && row < m) 
+    {
+        for(int i = 0; i < n; i++) 
+        {
             sum += a[row * n + i] * b[i * k + col];
         }
         c[row * k + col] = sum;
